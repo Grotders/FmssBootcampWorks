55476dd7a176a85b99651c5c7a005df0071436b4
Oguzcanbicer <oguzcanbicer96@gmail.com>
2022-09-16 14:05:12 +0300

    Refactor Week01 and Week02

--- Backend/src/Week01/Main.java
+++ Backend/src/Week01/Main.java
@@ -5,22 +5,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        odev1();
+        odev2();
+    }
 
 /*----------------------------------------------------------------------------------------------------------------------
-        Odev1
-            Sınıf Çalışması: Parametresi ile aldığı int türden 3 basamaklı bir sayının basamakları toplamını döngü
-        kullanmadan döndüren getDigitsSum metodunu yazınız. Metot sayının basamak sayısı kontrolünü yapmayacaktır
+    Odev1
+        Sınıf Çalışması: Parametresi ile aldığı int türden 3 basamaklı bir sayının basamakları toplamını döngü
+    kullanmadan döndüren getDigitsSum metodunu yazınız. Metot sayının basamak sayısı kontrolünü yapmayacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
+    public static void odev1() {
+        int result = getDigitSum(331);
+        System.out.println("result: " + result);
+    }
 
-//        int result = getDigitSum(331);
-//        System.out.println("result: " + result);
+    public static int getDigitSum(int number) {
+        // base case
+        if (number == 0)
+            return 0;
 
+        // recursive case
+        return number % 10 + getDigitSum(number / 10);
+    }
 /*----------------------------------------------------------------------------------------------------------------------
-        Odev2
-            Sınıf Çalışması: Katsayıları klavyeden girilen ikinci dereceden bir denklemin köklerini bulup ekrana
-        basan programı yazınız
+    Odev2
+        Sınıf Çalışması: Katsayıları klavyeden girilen ikinci dereceden bir denklemin köklerini bulup ekrana basan
+    programı yazınız
 ----------------------------------------------------------------------------------------------------------------------*/
-        rootFinder();
+    public static void odev2() {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("2. dereceden bir denklemin köklerini bulan program. (a(x^2) * bx + c)\n");
+        System.out.print("a: ");
+        int a = Integer.parseInt(keyboard.nextLine());
+        System.out.print("b: ");
+        int b = Integer.parseInt(keyboard.nextLine());
+        System.out.print("c: ");
+        int c = Integer.parseInt(keyboard.nextLine());
+
+        rootFinder(a, b, c);
+    }
 
 //        // △ > 0
 //        QuadraticEquation equation = new QuadraticEquation(1,-7,12);
@@ -33,31 +57,46 @@ public class Main {
 //        // △ < 0
 //        QuadraticEquation equation3 = new QuadraticEquation(3,3,5);
 //        equation3.rootFinder();
+//   a(x^2) * bx + c
+    public static void rootFinder(int a, int b, int c) {
+        String resultText = a + "(x^2) + " + b + "x + " + c + "\n";
+        int delta = delta(a, b, c);
+        double roots[] = bhaskaraFormula(a, b, delta);
+
+        if (delta > 0) {
+            resultText += "△: " + delta + "  -->  △ > 0  \ttwo distinct real root  (x1 ≠ x2)\n";
+        } else if (delta == 0){
+            resultText += "△: " + delta + "  -->  △ = 0  \ttwo same real root      (x1 = x2)\n";
+        } else {
+            resultText += "△: " + delta + "  -->  △ < 0  \tno real root\n";
+        }
+        resultText += "\nroot 1: " + roots[0];
+        resultText += "\nroot 2: " + roots[1] + "\n";
+
+        System.out.println(resultText);
     }
 
-    // a(x^2) * bx + c
-    public static void rootFinder() {
-        Scanner keyboard = new Scanner(System.in);
+    // delta(△) = (b^2) - 4ac
+    public static int delta(int a, int b, int c) {
+        int delta = (b * b) - (4 * a * c);
 
-        System.out.println("2. dereceden bir denklemin köklerini bulan program. (a(x^2) * bx + c)\n");
-        System.out.print("a: ");
-        int a = Integer.parseInt(keyboard.nextLine());
-        System.out.print("b: ");
-        int b = Integer.parseInt(keyboard.nextLine());
-        System.out.print("c: ");
-        int c = Integer.parseInt(keyboard.nextLine());
-        System.out.println();
-        QuadraticEquation equation = new QuadraticEquation(a, b, c);
-        equation.rootFinder();
+        return delta;
     }
 
-    public static int getDigitSum(int number) {
-        // base case
-        if (number == 0)
-            return 0;
+    /* bhaskara formula:
 
-        // recursive case
-        return number % 10 + getDigitSum(number / 10);
+             -b + √(delta)          -b - √(delta)
+         ---------------------, ---------------------
+                 2 * a                  2 * a
+
+     */
+    public static double[] bhaskaraFormula(int a, int b, int delta) {
+        double roots[] = new double[2];
+
+        roots[0] = ( -1 * b + Math.sqrt(delta)) / 2 * a;
+        roots[1] = ( -1 * b - Math.sqrt(delta)) / 2 * a;
+
+        return roots;
     }
 }
 
--- Backend/src/Week02/MatchDraw/Database.java
+++ Backend/src/Week02/MatchDraw/Database.java
@@ -1,12 +1,20 @@
 package Week02.MatchDraw;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.util.ArrayList;
 
 public class Database {
     private static final ArrayList<Club> allClubs = new ArrayList<>();
 
     static {
-        createAllClubsList();
+        getAllClubs();
     }
 
     public static ArrayList<Club> pickClubs(int howManyClubNeeded) {
@@ -18,57 +26,27 @@ public class Database {
         return clubs;
     }
 
-    private static void createAllClubsList() {
-        allClubs.add(new Club("Liverpool FC"));
-        allClubs.add(new Club("Manchester City"));
-        allClubs.add(new Club("Real Madrid"));
-        allClubs.add(new Club("Bayern München"));
-        allClubs.add(new Club("Inter Milan"));
-        allClubs.add(new Club("AC Milan"));
-        allClubs.add(new Club("Ajax Amsterdam"));
-        allClubs.add(new Club("Chelsea FC"));
-        allClubs.add(new Club("Paris Saint-Germain"));
-        allClubs.add(new Club("SSC Napoli"));
-        allClubs.add(new Club("Atletico Madrid"));
-        allClubs.add(new Club("Tottenham Hotspur"));
-        allClubs.add(new Club("Palmeiras"));
-        allClubs.add(new Club("Barcelona"));
-        allClubs.add(new Club("Villarreal"));
-        allClubs.add(new Club("Flamengo"));
-        allClubs.add(new Club("Borussia Dortmund"));
-        allClubs.add(new Club("Arsenal"));
-        allClubs.add(new Club("Benfica"));
-        allClubs.add(new Club("Juventus"));
-        allClubs.add(new Club("Sporting"));
-        allClubs.add(new Club("RB Leipzig"));
-        allClubs.add(new Club("FK Red Star Belgrade"));
-        allClubs.add(new Club("Sevilla"));
-        allClubs.add(new Club("Roma"));
-        allClubs.add(new Club("Bayer Leverkusen"));
-        allClubs.add(new Club("Lazio"));
-        allClubs.add(new Club("PSV Eindhoven"));
-        allClubs.add(new Club("Real Betis"));
-        allClubs.add(new Club("Rangers"));
-        allClubs.add(new Club("1. FC Union Berlin"));
-        allClubs.add(new Club("Red Bull Salzburg"));
-        allClubs.add(new Club("Monaco"));
-        allClubs.add(new Club("Atalanta"));
-        allClubs.add(new Club("Real Sociedad"));
-        allClubs.add(new Club("Atlético Mineiro"));
-        allClubs.add(new Club("Flora Tallinn"));
-        allClubs.add(new Club("Newcastle United"));
-        allClubs.add(new Club("Slavia Prague"));
-        allClubs.add(new Club("Czech Republic"));
-        allClubs.add(new Club("Shakhtar Donetsk"));
-        allClubs.add(new Club("Celtic"));
-        allClubs.add(new Club("Lyon"));
-        allClubs.add(new Club("Olympiakos"));
-        allClubs.add(new Club("River Plate"));
-        allClubs.add(new Club("Brighton"));
-        allClubs.add(new Club("Feyenoord"));
-        allClubs.add(new Club("Marseille"));
-        allClubs.add(new Club("Athletic Bilbao"));
-        allClubs.add(new Club("Freiburg"));
+    private static void getAllClubs() {
+        try{
+
+
+        File file = new File("backend\\src\\Week02\\MatchDraw\\clubs.xml");
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(file);
+        doc.getDocumentElement().normalize();
+        NodeList nodeList = doc.getElementsByTagName("club");
+
+            for (int itr = 0; itr < nodeList.getLength(); itr++) {
+                Node node = nodeList.item(itr);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    allClubs.add(new Club(eElement.getElementsByTagName("name").item(0).getTextContent()));
+                }
+            }
+         } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 }
 
